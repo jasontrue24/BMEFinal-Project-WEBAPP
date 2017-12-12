e31ba2baafea4eba7932f641bb77a65c3721f162
Jiang Baowei <jasontrue24@gmail.com>
2017-12-11 21:18:16 -0500

    new mian file

--- mian.py
+++ mian.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, world2'
+
+def dataconversion():
+    base64img="""we get from online"""
+    for i in base64img.keys():
+        img = base64img[i]
+        imgstring = base64.b64decode(img)
+        imgconverted = open('*****.jpg'.format(i), 'wb')
+        imgconverted.write(imgstring)
+
+
+        
