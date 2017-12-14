89a949f56eaebd0abb36db029c1ae3c754c6782a
Gina Lee <gi.na.lee@duke.edu>
2017-12-13 22:23:15 -0500

    stupid git - why does push not work

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,33 +1,44 @@
+import { Button } from 'react-bootstrap';
+import { ButtonToolbar } from 'react-bootstrap';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import DropzoneComponent from 'react-dropzone-component';
+//import DropzoneComponent from 'react-dropzone-component';
 import { UploadField } from '@navjobs/upload';
 import axios from 'axios'
+//import RaisedButton from 'material-ui/RaisedButton';
 
 class Upload extends Component {
 	constructor() {
-		super();		
+		super();
 		this.state = {
 			currentImageString: '',
+			resultString:[],
+			predictions : [],
 		}
 	} 
 
-	onUpload = (files) => {
+	
+        onUpload = (files) => {
 		const reader = new FileReader()
 		const file = files[0]
 		reader.readAsDataURL(file);
 		reader.onloadend = () => {
 			console.log(reader.result);
 			this.setState({currentImageString: reader.result});
+ axios.post('http://0.0.0.0:8000/api/images',{data: reader.result}).then( (response)=> {
+                        console.log("success!");
+                        console.log(response);
+                        this.setState({resultString: response.data.labels[0]});
+                        this.setState({predictions: Math.round(response.data.predictions[0]*100).toFixed(2)})
+                }).catch( (error)=> {
+                        console.log("failed to upload into api");
+                });
+
 		}
-		axios.post('http://vcm-1006.vm.duke.edu:5000/api/images',reader.result).then( (response)=> {
-			console.log("success!");
-		}).catch( (error)=> {
-			console.log("failed to upload into api");
-		});
-	}
+}
 	
-	render() {
+	
+render() {
 		return (
 			<div>
 				<h2>Upload your image</h2>	
@@ -41,6 +52,16 @@ class Upload extends Component {
 					</div>	
 				</UploadField>
 				<img src={this.state.currentImageString} />
+
+  <ButtonToolbar>
+    <Button bsStyle="primary" bsSize="large" active>Here is the result</Button>
+    <Button bsSize="large" active>Button</Button>
+  </ButtonToolbar>
+
+
+ReactDOM.render(buttonsInstance, mountNode);
+
+
 			</div>
 		)
 	}
