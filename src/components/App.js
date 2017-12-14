2cdda7d573da4828108e44182cbae3c061399dda
qz58 <31520213+qidizhai@users.noreply.github.com>
2017-12-13 22:25:55 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import Upload from './Upload';
-import Sites from './Sites';
 
 class App extends Component {
   render() {
@@ -11,7 +10,7 @@ class App extends Component {
 	  	<MuiThemeProvider>
 			<AppBar title="Melanoma Detector" showMenuIconButton={false}/>
 			<Upload />
-			<Sites />
+			
 		
 		</MuiThemeProvider>
       </div>
